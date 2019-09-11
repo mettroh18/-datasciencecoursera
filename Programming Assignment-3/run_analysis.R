bf6bf533708afa5c4182171a1ea99aeaff3caebe
Rohini Mettu <ro.mettu@gmail.com>
2019-09-11 16:27:32 -0700

    new submission

--- Programming Assignment-3/run_analysis.R
+++ Programming Assignment-3/run_analysis.R
@@ -7,36 +7,41 @@ library(readr)
 library(readtext)
 
 
-if(!file.exists("/dataset")) {dir.create("./dataset")}
-fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileURL, destfile = "./data/downloads.csv", method = "curl")
-
-
-train1 <- list.files(path="./data/train/Inertial-Signals", pattern="body_acc", full.names=TRUE)
-train2 <- list.files(path="./data/train/Inertial-Signals", pattern="body_gyro", full.names=TRUE)
-train3 <- list.files(path="./data/train/Inertial-Signals", pattern="total_acc", full.names=TRUE)
-
-trainA <- merge(train1, by.x = "", by.y = "", all = TRUE)
-trainB <- merge(train2, by.x = "", by.y = "", all = TRUE)
-trainC <- merge(train3, by.x = "", by.y = "", all = TRUE)
-
-
 #Merges the training and the test sets to create one data set.
 training <- readtext("./data/train/X_train.txt")
 test <- readtext("./data/test/X_test.txt")
 
-merged <- data.frame(training, test)
+dataset <- rbind(training, test)
+features <- read.table("data/features.txt")$V2
+names(dataset) <- features
 
 #Extracts only the measurements on the mean and standard deviation for each measurement.
+filtered <- grepl("std|mean", features) & !grepl("meanFreq", features)
+dataset <- dataset[filtered]
+rm(features, filtered)
 
-
+#Label each row by its appropriate subject
+subjects1 <- read.table("data/train/subject_train.txt")
+subjects2 <- read.table("data/test/subject_test.txt")
+allsubjects <- rbind(subjects1, subjects2)
+names(labels) <- c("Subject")
+dataset <- cbind(allsubjects, dataset)
+rm(subjects1, subjects2, allsubjects)
 
 #Uses descriptive activity names to name the activities in the data set
+labels1 <- read.table("data/train/y_train.txt")
+labels2 <- read.table("data/test/y_test.txt")
+activities <- rbind(labels1, labels2) 
 
-
-
-#Appropriately labels the data set with descriptive variable names.
-
-
+code <- read.table("data/activity_labels.txt")
+activities <- (lapply(activities, function(x) code[x,2]))
+activities <- as.data.frame(activities)
+names(activities) <- c("Activity")
+dataset <- cbind(activities, dataset)
+rm(labels1, labels2, code, activities)
 
 #From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+tidydata <- dataset %>%
+  group_by(Activity, Subject) %>%
+  summarize_all(funs(mean))
+write.table(tidy, file = "./tidy.txt", row.names = FALSE)
